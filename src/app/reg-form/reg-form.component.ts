6cf5f7c5343c67a2d22b10fb1461034569987f9b
Gunjanrawat5 <gunjanrawat5@gmail.com>
2022-12-08 00:01:25 +0530

    Validation ongoing

--- src/app/reg-form/reg-form.component.ts
+++ src/app/reg-form/reg-form.component.ts
@@ -1,4 +1,6 @@
+import { formatCurrency } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-reg-form',
@@ -7,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegFormComponent implements OnInit {
 
+  mobileNo =''
+  email=''
+  PANcard = ''
+  Aadhar=''
   constructor() { }
 
   ngOnInit(): void {
   }
 
   validate(){
-    
+    document.write(this.mobileNo + "" + this.email + "" + this.PANcard + "" + this.Aadhar)
   }
 
 }
